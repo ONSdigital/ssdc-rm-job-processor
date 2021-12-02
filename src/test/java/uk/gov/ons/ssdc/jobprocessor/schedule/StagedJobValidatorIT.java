ad3c3c3620f67dc9f453ebe9a8dc29a34a4309d0
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-12-02 09:57:44 +0000

    Integration test more scenarios

--- src/test/java/uk/gov/ons/ssdc/jobprocessor/schedule/StagedJobValidatorIT.java
+++ src/test/java/uk/gov/ons/ssdc/jobprocessor/schedule/StagedJobValidatorIT.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.ons.ssdc.common.model.entity.Case;
 import uk.gov.ons.ssdc.common.model.entity.CollectionExercise;
 import uk.gov.ons.ssdc.common.model.entity.Job;
 import uk.gov.ons.ssdc.common.model.entity.JobRow;
@@ -53,7 +54,7 @@ public class StagedJobValidatorIT {
     jobRow.setId(UUID.randomUUID());
     jobRow.setJob(job);
     jobRow.setJobRowStatus(JobRowStatus.STAGED);
-    jobRow.setRowData(Map.of("Junk", "test junk", "SensitiveJunk", "sensitive test junk"));
+    jobRow.setRowData(Map.of("Junk", "test junk", "SensitiveJunk", "upadte"));
     jobRow.setOriginalRowData(new String[] {"foo", "bar"});
     jobRowRepository.saveAndFlush(jobRow);
 
@@ -106,7 +107,7 @@ public class StagedJobValidatorIT {
     jobRow.setId(UUID.randomUUID());
     jobRow.setJob(job);
     jobRow.setJobRowStatus(JobRowStatus.STAGED);
-    jobRow.setRowData(Map.of("Junk", "", "SensitiveJunk", "sensitive test junk"));
+    jobRow.setRowData(Map.of("Junk", "", "SensitiveJunk", "update"));
     jobRow.setOriginalRowData(new String[] {"foo", "bar"});
     jobRowRepository.saveAndFlush(jobRow);
 
@@ -140,5 +141,299 @@ public class StagedJobValidatorIT {
     assertThat(processedJobRow.getJobRowStatus()).isEqualTo(JobRowStatus.VALIDATED_ERROR);
     assertThat(processedJobRow.getValidationErrorDescriptions())
         .isEqualTo("Column 'Junk' value '' validation error: Mandatory value missing");
+  }
+
+  @Test
+  void processStagedJobsBulkUpdateSample() {
+    Case caze = junkDataHelper.setupJunkCase();
+    CollectionExercise collectionExercise = caze.getCollectionExercise();
+
+    Job job = new Job();
+    job.setId(UUID.randomUUID());
+    job.setCollectionExercise(collectionExercise);
+    job.setJobStatus(JobStatus.STAGING_IN_PROGRESS);
+    job.setJobType(JobType.BULK_UPDATE_SAMPLE);
+    job.setCreatedBy("norman");
+    job.setCreatedAt(OffsetDateTime.now());
+    job.setFileId(UUID.randomUUID());
+    job.setFileName("normansfile.csv");
+    job = jobRepository.saveAndFlush(job);
+
+    JobRow jobRow = new JobRow();
+    jobRow.setId(UUID.randomUUID());
+    jobRow.setJob(job);
+    jobRow.setJobRowStatus(JobRowStatus.STAGED);
+    jobRow.setRowData(
+        Map.of(
+            "caseId", caze.getId().toString(), "fieldToUpdate", "Junk", "newValue", "test update"));
+    jobRow.setOriginalRowData(new String[] {"foo", "bar"});
+    jobRowRepository.saveAndFlush(jobRow);
+
+    // This will unleash the hounds
+    job.setJobStatus(JobStatus.VALIDATION_IN_PROGRESS);
+    jobRepository.saveAndFlush(job);
+
+    // Now check that the job processed OK
+    LocalTime testTimeout = LocalTime.now().plusSeconds(60);
+
+    Job processedJob = null;
+
+    do {
+      if (processedJob != null) {
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          // Ignored
+        }
+      }
+
+      processedJob = jobRepository.findById(job.getId()).get();
+    } while (processedJob.getJobStatus() == JobStatus.VALIDATION_IN_PROGRESS
+        && LocalTime.now().isBefore(testTimeout));
+
+    assertThat(processedJob.getJobStatus()).isEqualTo(JobStatus.VALIDATED_OK);
+    assertThat(processedJob.getValidatingRowNumber()).isEqualTo(1);
+    assertThat(processedJob.getErrorRowCount()).isEqualTo(0);
+
+    JobRow processedJobRow = jobRowRepository.findById(jobRow.getId()).get();
+    assertThat(processedJobRow.getJobRowStatus()).isEqualTo(JobRowStatus.VALIDATED_OK);
+  }
+
+  @Test
+  void processStagedJobsBulkUpdateSampleFailsValidation() {
+    Case caze = junkDataHelper.setupJunkCase();
+    CollectionExercise collectionExercise = caze.getCollectionExercise();
+
+    Job job = new Job();
+    job.setId(UUID.randomUUID());
+    job.setCollectionExercise(collectionExercise);
+    job.setJobStatus(JobStatus.STAGING_IN_PROGRESS);
+    job.setJobType(JobType.BULK_UPDATE_SAMPLE);
+    job.setCreatedBy("norman");
+    job.setCreatedAt(OffsetDateTime.now());
+    job.setFileId(UUID.randomUUID());
+    job.setFileName("normansfile.csv");
+    job = jobRepository.saveAndFlush(job);
+
+    JobRow jobRow = new JobRow();
+    jobRow.setId(UUID.randomUUID());
+    jobRow.setJob(job);
+    jobRow.setJobRowStatus(JobRowStatus.STAGED);
+    jobRow.setRowData(
+        Map.of("caseId", caze.getId().toString(), "fieldToUpdate", "Junk", "newValue", ""));
+    jobRow.setOriginalRowData(new String[] {"foo", "bar"});
+    jobRowRepository.saveAndFlush(jobRow);
+
+    // This will unleash the hounds
+    job.setJobStatus(JobStatus.VALIDATION_IN_PROGRESS);
+    jobRepository.saveAndFlush(job);
+
+    // Now check that the job processed OK
+    LocalTime testTimeout = LocalTime.now().plusSeconds(60);
+
+    Job processedJob = null;
+
+    do {
+      if (processedJob != null) {
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          // Ignored
+        }
+      }
+
+      processedJob = jobRepository.findById(job.getId()).get();
+    } while (processedJob.getJobStatus() == JobStatus.VALIDATION_IN_PROGRESS
+        && LocalTime.now().isBefore(testTimeout));
+
+    assertThat(processedJob.getJobStatus()).isEqualTo(JobStatus.VALIDATED_WITH_ERRORS);
+    assertThat(processedJob.getValidatingRowNumber()).isEqualTo(1);
+    assertThat(processedJob.getErrorRowCount()).isEqualTo(1);
+
+    JobRow processedJobRow = jobRowRepository.findById(jobRow.getId()).get();
+    assertThat(processedJobRow.getJobRowStatus()).isEqualTo(JobRowStatus.VALIDATED_ERROR);
+    assertThat(processedJobRow.getValidationErrorDescriptions())
+        .isEqualTo("Column 'newValue' value '' validation error: Mandatory value missing");
+  }
+
+  @Test
+  void processStagedJobsBulkUpdateSampleSensitive() {
+    Case caze = junkDataHelper.setupJunkCase();
+    CollectionExercise collectionExercise = caze.getCollectionExercise();
+
+    Job job = new Job();
+    job.setId(UUID.randomUUID());
+    job.setCollectionExercise(collectionExercise);
+    job.setJobStatus(JobStatus.STAGING_IN_PROGRESS);
+    job.setJobType(JobType.BULK_UPDATE_SAMPLE_SENSITIVE);
+    job.setCreatedBy("norman");
+    job.setCreatedAt(OffsetDateTime.now());
+    job.setFileId(UUID.randomUUID());
+    job.setFileName("normansfile.csv");
+    job = jobRepository.saveAndFlush(job);
+
+    JobRow jobRow = new JobRow();
+    jobRow.setId(UUID.randomUUID());
+    jobRow.setJob(job);
+    jobRow.setJobRowStatus(JobRowStatus.STAGED);
+    jobRow.setRowData(
+        Map.of(
+            "caseId",
+            caze.getId().toString(),
+            "fieldToUpdate",
+            "SensitiveJunk",
+            "newValue",
+            "update"));
+    jobRow.setOriginalRowData(new String[] {"foo", "bar"});
+    jobRowRepository.saveAndFlush(jobRow);
+
+    // This will unleash the hounds
+    job.setJobStatus(JobStatus.VALIDATION_IN_PROGRESS);
+    jobRepository.saveAndFlush(job);
+
+    // Now check that the job processed OK
+    LocalTime testTimeout = LocalTime.now().plusSeconds(60);
+
+    Job processedJob = null;
+
+    do {
+      if (processedJob != null) {
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          // Ignored
+        }
+      }
+
+      processedJob = jobRepository.findById(job.getId()).get();
+    } while (processedJob.getJobStatus() == JobStatus.VALIDATION_IN_PROGRESS
+        && LocalTime.now().isBefore(testTimeout));
+
+    assertThat(processedJob.getJobStatus()).isEqualTo(JobStatus.VALIDATED_OK);
+    assertThat(processedJob.getValidatingRowNumber()).isEqualTo(1);
+    assertThat(processedJob.getErrorRowCount()).isEqualTo(0);
+
+    JobRow processedJobRow = jobRowRepository.findById(jobRow.getId()).get();
+    assertThat(processedJobRow.getJobRowStatus()).isEqualTo(JobRowStatus.VALIDATED_OK);
+  }
+
+  @Test
+  void processStagedJobsBulkUpdateSampleSensitiveBlankingAllowed() {
+    Case caze = junkDataHelper.setupJunkCase();
+    CollectionExercise collectionExercise = caze.getCollectionExercise();
+
+    Job job = new Job();
+    job.setId(UUID.randomUUID());
+    job.setCollectionExercise(collectionExercise);
+    job.setJobStatus(JobStatus.STAGING_IN_PROGRESS);
+    job.setJobType(JobType.BULK_UPDATE_SAMPLE_SENSITIVE);
+    job.setCreatedBy("norman");
+    job.setCreatedAt(OffsetDateTime.now());
+    job.setFileId(UUID.randomUUID());
+    job.setFileName("normansfile.csv");
+    job = jobRepository.saveAndFlush(job);
+
+    JobRow jobRow = new JobRow();
+    jobRow.setId(UUID.randomUUID());
+    jobRow.setJob(job);
+    jobRow.setJobRowStatus(JobRowStatus.STAGED);
+    jobRow.setRowData(
+        Map.of(
+            "caseId", caze.getId().toString(), "fieldToUpdate", "SensitiveJunk", "newValue", ""));
+    jobRow.setOriginalRowData(new String[] {"foo", "bar"});
+    jobRowRepository.saveAndFlush(jobRow);
+
+    // This will unleash the hounds
+    job.setJobStatus(JobStatus.VALIDATION_IN_PROGRESS);
+    jobRepository.saveAndFlush(job);
+
+    // Now check that the job processed OK
+    LocalTime testTimeout = LocalTime.now().plusSeconds(60);
+
+    Job processedJob = null;
+
+    do {
+      if (processedJob != null) {
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          // Ignored
+        }
+      }
+
+      processedJob = jobRepository.findById(job.getId()).get();
+    } while (processedJob.getJobStatus() == JobStatus.VALIDATION_IN_PROGRESS
+        && LocalTime.now().isBefore(testTimeout));
+
+    assertThat(processedJob.getJobStatus()).isEqualTo(JobStatus.VALIDATED_OK);
+    assertThat(processedJob.getValidatingRowNumber()).isEqualTo(1);
+    assertThat(processedJob.getErrorRowCount()).isEqualTo(0);
+
+    JobRow processedJobRow = jobRowRepository.findById(jobRow.getId()).get();
+    assertThat(processedJobRow.getJobRowStatus()).isEqualTo(JobRowStatus.VALIDATED_OK);
+  }
+
+  @Test
+  void processStagedJobsBulkUpdateSampleSensitiveFailsValidation() {
+    Case caze = junkDataHelper.setupJunkCase();
+    CollectionExercise collectionExercise = caze.getCollectionExercise();
+
+    Job job = new Job();
+    job.setId(UUID.randomUUID());
+    job.setCollectionExercise(collectionExercise);
+    job.setJobStatus(JobStatus.STAGING_IN_PROGRESS);
+    job.setJobType(JobType.BULK_UPDATE_SAMPLE_SENSITIVE);
+    job.setCreatedBy("norman");
+    job.setCreatedAt(OffsetDateTime.now());
+    job.setFileId(UUID.randomUUID());
+    job.setFileName("normansfile.csv");
+    job = jobRepository.saveAndFlush(job);
+
+    JobRow jobRow = new JobRow();
+    jobRow.setId(UUID.randomUUID());
+    jobRow.setJob(job);
+    jobRow.setJobRowStatus(JobRowStatus.STAGED);
+    jobRow.setRowData(
+        Map.of(
+            "caseId",
+            caze.getId().toString(),
+            "fieldToUpdate",
+            "SensitiveJunk",
+            "newValue",
+            "1234567890123"));
+    jobRow.setOriginalRowData(new String[] {"foo", "bar"});
+    jobRowRepository.saveAndFlush(jobRow);
+
+    // This will unleash the hounds
+    job.setJobStatus(JobStatus.VALIDATION_IN_PROGRESS);
+    jobRepository.saveAndFlush(job);
+
+    // Now check that the job processed OK
+    LocalTime testTimeout = LocalTime.now().plusSeconds(60);
+
+    Job processedJob = null;
+
+    do {
+      if (processedJob != null) {
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          // Ignored
+        }
+      }
+
+      processedJob = jobRepository.findById(job.getId()).get();
+    } while (processedJob.getJobStatus() == JobStatus.VALIDATION_IN_PROGRESS
+        && LocalTime.now().isBefore(testTimeout));
+
+    assertThat(processedJob.getJobStatus()).isEqualTo(JobStatus.VALIDATED_WITH_ERRORS);
+    assertThat(processedJob.getValidatingRowNumber()).isEqualTo(1);
+    assertThat(processedJob.getErrorRowCount()).isEqualTo(1);
+
+    JobRow processedJobRow = jobRowRepository.findById(jobRow.getId()).get();
+    assertThat(processedJobRow.getJobRowStatus()).isEqualTo(JobRowStatus.VALIDATED_ERROR);
+    assertThat(processedJobRow.getValidationErrorDescriptions())
+        .isEqualTo(
+            "Column 'newValue' value '1234567890123' validation error: Exceeded max length of 10");
   }
 }
--- src/test/java/uk/gov/ons/ssdc/jobprocessor/testutils/JunkDataHelper.java
+++ src/test/java/uk/gov/ons/ssdc/jobprocessor/testutils/JunkDataHelper.java
@@ -1,24 +1,45 @@
 package uk.gov.ons.ssdc.jobprocessor.testutils;
 
 import java.time.OffsetDateTime;
+import java.util.Map;
+import java.util.Random;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.ActiveProfiles;
+import uk.gov.ons.ssdc.common.model.entity.Case;
 import uk.gov.ons.ssdc.common.model.entity.CollectionExercise;
 import uk.gov.ons.ssdc.common.model.entity.CollectionInstrumentSelectionRule;
 import uk.gov.ons.ssdc.common.model.entity.Survey;
 import uk.gov.ons.ssdc.common.validation.ColumnValidator;
+import uk.gov.ons.ssdc.common.validation.LengthRule;
 import uk.gov.ons.ssdc.common.validation.MandatoryRule;
 import uk.gov.ons.ssdc.common.validation.Rule;
+import uk.gov.ons.ssdc.jobprocessor.repository.CaseRepository;
 import uk.gov.ons.ssdc.jobprocessor.repository.CollectionExerciseRepository;
 import uk.gov.ons.ssdc.jobprocessor.repository.SurveyRepository;
 
 @Component
 @ActiveProfiles("test")
 public class JunkDataHelper {
+  private static final Random RANDOM = new Random();
+
+  @Autowired private CaseRepository caseRepository;
   @Autowired private CollectionExerciseRepository collectionExerciseRepository;
   @Autowired private SurveyRepository surveyRepository;
+
+  public Case setupJunkCase() {
+    Case junkCase = new Case();
+    junkCase.setId(UUID.randomUUID());
+    junkCase.setInvalid(false);
+    junkCase.setCollectionExercise(setupJunkCollex());
+    junkCase.setCaseRef(RANDOM.nextLong());
+    junkCase.setSample(Map.of("Junk", "foo"));
+    junkCase.setSampleSensitive(Map.of("SensitiveJunk", "bar"));
+    caseRepository.save(junkCase);
+
+    return junkCase;
+  }
 
   public CollectionExercise setupJunkCollex() {
     Survey junkSurvey = new Survey();
@@ -27,7 +48,8 @@ public class JunkDataHelper {
     junkSurvey.setSampleValidationRules(
         new ColumnValidator[] {
           new ColumnValidator("Junk", false, new Rule[] {new MandatoryRule()}),
-          new ColumnValidator("SensitiveJunk", true, new Rule[] {new MandatoryRule()})
+          new ColumnValidator(
+              "SensitiveJunk", true, new Rule[] {new MandatoryRule(), new LengthRule(10)})
         });
     junkSurvey.setSampleSeparator('j');
     junkSurvey.setSampleDefinitionUrl("http://junk");
