1e367775f7065e21d9a9e30a4024255e567eaf5c
LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
2022-01-25 14:17:02 +0000

    use correct columns for bulk and invalid (#18)

--- src/main/java/uk/gov/ons/ssdc/jobprocessor/jobtype/processors/BulkRefusalTypeProcessor.java
+++ src/main/java/uk/gov/ons/ssdc/jobprocessor/jobtype/processors/BulkRefusalTypeProcessor.java
@@ -2,11 +2,17 @@ package uk.gov.ons.ssdc.jobprocessor.jobtype.processors;
 
 import static com.google.cloud.spring.pubsub.support.PubSubTopicUtils.toProjectTopicName;
 
+import java.util.EnumSet;
 import uk.gov.ons.ssdc.common.model.entity.CollectionExercise;
 import uk.gov.ons.ssdc.common.model.entity.JobType;
 import uk.gov.ons.ssdc.common.model.entity.UserGroupAuthorisedActivityType;
+import uk.gov.ons.ssdc.common.validation.ColumnValidator;
+import uk.gov.ons.ssdc.common.validation.InSetRule;
+import uk.gov.ons.ssdc.common.validation.Rule;
+import uk.gov.ons.ssdc.jobprocessor.model.dto.messaging.RefusalTypeDTO;
 import uk.gov.ons.ssdc.jobprocessor.transformer.BulkRefusalTransformer;
 import uk.gov.ons.ssdc.jobprocessor.transformer.Transformer;
+import uk.gov.ons.ssdc.jobprocessor.validators.CaseExistsInCollectionExerciseRule;
 
 public class BulkRefusalTypeProcessor extends JobTypeProcessor {
   private static final Transformer BULK_REFUSAL_TRANSFORMER = new BulkRefusalTransformer();
@@ -15,9 +21,24 @@ public class BulkRefusalTypeProcessor extends JobTypeProcessor {
       String topic, String sharedPubsubProject, CollectionExercise collectionExercise) {
     setJobType(JobType.BULK_REFUSAL);
     setTransformer(BULK_REFUSAL_TRANSFORMER);
-    setColumnValidators(collectionExercise.getSurvey().getSampleValidationRules());
+    setColumnValidators(getBulkRefusalProcessorValidationRules(collectionExercise));
     setTopic(toProjectTopicName(topic, sharedPubsubProject).toString());
     setFileLoadPermission(UserGroupAuthorisedActivityType.LOAD_BULK_REFUSAL);
     setFileViewProgressPermission(UserGroupAuthorisedActivityType.VIEW_BULK_REFUSAL_PROGRESS);
+  }
+
+  private ColumnValidator[] getBulkRefusalProcessorValidationRules(
+      CollectionExercise collectionExercise) {
+    Rule[] caseExistsRules = {new CaseExistsInCollectionExerciseRule(collectionExercise)};
+    ColumnValidator caseExistsValidator = new ColumnValidator("caseId", false, caseExistsRules);
+
+    String[] refusalTypes =
+        EnumSet.allOf(RefusalTypeDTO.class).stream().map(Enum::toString).toArray(String[]::new);
+    Rule[] refusalSetRules = {new InSetRule(refusalTypes)};
+
+    ColumnValidator refusalTypeValidator =
+        new ColumnValidator("refusalType", false, refusalSetRules);
+
+    return new ColumnValidator[] {caseExistsValidator, refusalTypeValidator};
   }
 }
